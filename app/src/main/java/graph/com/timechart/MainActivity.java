29fdee3b3775ba83ed9cd1454d851cc708b9c949
subham mishra <smsubham342@gmail.com>
2016-08-03 17:13:53 +0530

    shifted getdata()

--- app/src/main/java/graph/com/timechart/MainActivity.java
+++ app/src/main/java/graph/com/timechart/MainActivity.java
@@ -7,13 +7,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
@@ -21,7 +17,6 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.AxisValueFormatter;
 import com.github.mikephil.charting.formatter.FormattedStringCache;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.json.JSONArray;
@@ -32,19 +27,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-public class MainActivity extends DemoBase implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends DemoBase {
 
-    private LineChart mChart;
-    private SeekBar mSeekBarX;
-    private TextView tvX;
+    private LineChart chart;
     ArrayList<Float> data = new ArrayList<>();  //used to store timestamp corresponding value in getChartData class i.e. 303 for 1467871629
-    JSONObject jsonObject;
-    JSONArray jsonArray1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -53,47 +44,18 @@ public class MainActivity extends DemoBase implements SeekBar.OnSeekBarChangeLis
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_linechart_time);
 
-        tvX = (TextView) findViewById(R.id.tvXMax);
-        mSeekBarX = (SeekBar) findViewById(R.id.seekBar1);
-        mSeekBarX.setProgress(100);
-        tvX.setText("100");
 
-        mSeekBarX.setOnSeekBarChangeListener(this);
 
-        mChart = (LineChart) findViewById(R.id.chart1);
 
-        // no description text
-        mChart.setDescription("");
-        mChart.setNoDataTextDescription("You need to provide data for the chart.");
 
-        // enable touch gestures
-        mChart.setTouchEnabled(true);
+      try {
+          // add data
+          setData();
+      }
 
-        mChart.setDragDecelerationFrictionCoef(0.9f);
+        catch (JSONException e){}
 
-        // enable scaling and dragging
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        mChart.setDrawGridBackground(false);
-        mChart.setHighlightPerDragEnabled(true);
-
-        // set an alternative background color
-        mChart.setBackgroundColor(Color.WHITE);
-        mChart.setViewPortOffsets(0f, 0f, 0f, 0f);
-try{
-    // add data
-    setData();
-}
-catch (JSONException e){}
-        // add data
-        //setData();
-        mChart.invalidate();
-
-        // get the legend (only possible after setting data)
-        Legend l = mChart.getLegend();
-        l.setEnabled(false);
-
-        XAxis xAxis = mChart.getXAxis();
+        XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
         xAxis.setTypeface(mTfLight);
         xAxis.setTextSize(10f);
@@ -119,7 +81,7 @@ catch (JSONException e){}
             }
         });
 
-        YAxis leftAxis = mChart.getAxisLeft();
+        YAxis leftAxis = chart.getAxisLeft();
         leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
         leftAxis.setTypeface(mTfLight);
         leftAxis.setTextColor(ColorTemplate.getHoloBlue());
@@ -130,7 +92,7 @@ catch (JSONException e){}
         leftAxis.setYOffset(-9f);
         leftAxis.setTextColor(Color.rgb(255, 192, 56));
 
-        YAxis rightAxis = mChart.getAxisRight();
+        YAxis rightAxis = chart.getAxisRight();
         rightAxis.setEnabled(false);
     }
 
@@ -142,237 +104,19 @@ catch (JSONException e){}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.actionToggleValues: {
-                List<ILineDataSet> sets = mChart.getData()
-                        .getDataSets();
-
-                for (ILineDataSet iSet : sets) {
-
-                    LineDataSet set = (LineDataSet) iSet;
-                    set.setDrawValues(!set.isDrawValuesEnabled());
-                }
-
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionToggleHighlight: {
-                if (mChart.getData() != null) {
-                    mChart.getData().setHighlightEnabled(!mChart.getData().isHighlightEnabled());
-                    mChart.invalidate();
-                }
-                break;
-            }
-            case R.id.actionToggleFilled: {
-
-                List<ILineDataSet> sets = mChart.getData()
-                        .getDataSets();
-
-                for (ILineDataSet iSet : sets) {
-
-                    LineDataSet set = (LineDataSet) iSet;
-                    if (set.isDrawFilledEnabled())
-                        set.setDrawFilled(false);
-                    else
-                        set.setDrawFilled(true);
-                }
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionToggleCircles: {
-                List<ILineDataSet> sets = mChart.getData()
-                        .getDataSets();
-
-                for (ILineDataSet iSet : sets) {
-
-                    LineDataSet set = (LineDataSet) iSet;
-                    if (set.isDrawCirclesEnabled())
-                        set.setDrawCircles(false);
-                    else
-                        set.setDrawCircles(true);
-                }
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionToggleCubic: {
-                List<ILineDataSet> sets = mChart.getData()
-                        .getDataSets();
-
-                for (ILineDataSet iSet : sets) {
-
-                    LineDataSet set = (LineDataSet) iSet;
-                    if (set.getMode() == LineDataSet.Mode.CUBIC_BEZIER)
-                        set.setMode(LineDataSet.Mode.LINEAR);
-                    else
-                        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-                }
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionToggleStepped: {
-                List<ILineDataSet> sets = mChart.getData()
-                        .getDataSets();
-
-                for (ILineDataSet iSet : sets) {
-
-                    LineDataSet set = (LineDataSet) iSet;
-                    if (set.getMode() == LineDataSet.Mode.STEPPED)
-                        set.setMode(LineDataSet.Mode.LINEAR);
-                    else
-                        set.setMode(LineDataSet.Mode.STEPPED);
-                }
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionTogglePinch: {
-                if (mChart.isPinchZoomEnabled())
-                    mChart.setPinchZoom(false);
-                else
-                    mChart.setPinchZoom(true);
-
-                mChart.invalidate();
-                break;
-            }
-            case R.id.actionToggleAutoScaleMinMax: {
-                mChart.setAutoScaleMinMaxEnabled(!mChart.isAutoScaleMinMaxEnabled());
-                mChart.notifyDataSetChanged();
-                break;
-            }
-            case R.id.animateX: {
-                mChart.animateX(3000);
-                break;
-            }
-            case R.id.animateY: {
-                mChart.animateY(3000);
-                break;
-            }
-            case R.id.animateXY: {
-                mChart.animateXY(3000, 3000);
-                break;
-            }
-
-            case R.id.actionSave: {
-                if (mChart.saveToPath("title" + System.currentTimeMillis(), "")) {
-                    Toast.makeText(getApplicationContext(), "Saving SUCCESSFUL!",
-                            Toast.LENGTH_SHORT).show();
-                } else
-                    Toast.makeText(getApplicationContext(), "Saving FAILED!", Toast.LENGTH_SHORT)
-                            .show();
-
-                // mChart.saveToGallery("title"+System.currentTimeMillis())
-                break;
-            }
-        }
         return true;
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        tvX.setText("" + (mSeekBarX.getProgress()));
-
-        //setData(mSeekBarX.getProgress(), 50);
-
-        // redraw
-        mChart.invalidate();
-    }
     private void setData() throws JSONException{
         getChartData get_data=new getChartData();
-
         get_data.execute("");
-
-
-        ArrayList<Float> listdata = new ArrayList<>(); //timestamp data for graph
-       // JSONArray jArray = (JSONArray)jsonObject;
-        if (jsonArray1 != null) {
-            for (int i=0;i<jsonArray1.length();i++){
-                listdata.add(Float.parseFloat(jsonArray1.get(i).toString()));
-            }
-        }
-
-        ArrayList<Entry> values = new ArrayList<Entry>(); //data for y-axis
-        for (int i=0;i<data.size();i++){
-            values.add(new Entry(listdata.get(i),data.get(i))); //adding data for x and y axis respectively
-        }
-        //values.addAll(new Entry());
-        //Iterate the jsonArray and print the info of JSONObjects
-
-        //catch (JSONException e) {e.printStackTrace();}
-        //values.addAll(s);
-        // create a dataset and give it a type
-        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
-        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set1.setColor(ColorTemplate.getHoloBlue());
-        set1.setValueTextColor(ColorTemplate.getHoloBlue());
-        set1.setLineWidth(1.5f);
-        set1.setDrawCircles(false);
-        set1.setDrawValues(false);
-        set1.setFillAlpha(65);
-        set1.setFillColor(ColorTemplate.getHoloBlue());
-        set1.setHighLightColor(Color.rgb(244, 117, 117));
-        set1.setDrawCircleHole(false);
-
-        // create a data object with the datasets
-        LineData data = new LineData(set1);
-        data.setValueTextColor(Color.WHITE);
-        data.setValueTextSize(9f);
-
-        // set data
-        mChart.setData(data);
-
     }
 
-   /** private void setData(int count, float range) {
-
-        long now = System.currentTimeMillis();
-        long hourMillis = 3600000L;
-
-        ArrayList<Entry> values = new ArrayList<Entry>();
-
-        float from = now - (count / 2) * hourMillis;
-        float to = now + (count / 2) * hourMillis;
-
-        for (float x = from; x < to; x += hourMillis) {
-
-            float y = getRandom(range, 50);
-            values.add(new Entry(x, y)); // add one entry per hour
-        }
-
-        // create a dataset and give it a type
-        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
-        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set1.setColor(ColorTemplate.getHoloBlue());
-        set1.setValueTextColor(ColorTemplate.getHoloBlue());
-        set1.setLineWidth(1.5f);
-        set1.setDrawCircles(false);
-        set1.setDrawValues(false);
-        set1.setFillAlpha(65);
-        set1.setFillColor(ColorTemplate.getHoloBlue());
-        set1.setHighLightColor(Color.rgb(244, 117, 117));
-        set1.setDrawCircleHole(false);
-
-        // create a data object with the datasets
-        LineData data = new LineData(set1);
-        data.setValueTextColor(Color.WHITE);
-        data.setValueTextSize(9f);
-
-        // set data
-        mChart.setData(data);
-    } **/
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-        // TODO Auto-generated method stub
-
-    }
     private class getChartData extends AsyncTask<String, Void, String> {
         int len;
+        ArrayList<String> listdata; //timestamp data for graph
+        ArrayList<Float> listvalue; //corresponding date data value
+        ArrayList<Entry> values;
         private final String LOG_TAG = getChartData.class.getSimpleName();
 
         protected String doInBackground(String... params) {
@@ -380,7 +124,7 @@ catch (JSONException e){}
         }
 
         protected String getData() {
-            String decodedString = "";
+            String decodedString ="";
             String returnMsg = "";
             String request = "http://52.77.220.93:4000/getLast?device=thane1&sensor=arduino&lim=300";
             URL url;
@@ -388,6 +132,7 @@ catch (JSONException e){}
             JSONObject myJson=null;
             String JsonStr = null; //used to store json string
             JSONArray jsonArray=null; //used to store json data as json array
+            String prsdData[];
 
             try {
                 url = new URL(request);
@@ -399,65 +144,22 @@ catch (JSONException e){}
 
                 // Read the input stream into a String
                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                //checking if nothing is received
-                if (in== null) {
-                    // Nothing to do.
-                    return null;
-                }
-                StringBuffer buffer = new StringBuffer();//used to store json string as buffer
-                String line;
-                while ((line = in.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
 
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                JsonStr = buffer.toString();
-
-                //trying to display received data in Log.v
-                Log.v(LOG_TAG, "Forecast string: " + JsonStr);
-
-                //Creates a new JSONObject with name/value mappings from the JSON string.
-                jsonObject =new JSONObject(JsonStr);
-                jsonArray1=jsonObject.optJSONArray("data");
-
-                //gets all time stamp value as array of strings
-                JSONObject json2 = jsonObject.getJSONObject("data");
-                JSONArray jsonArray_keys=json2.names();
-
-                /**while ((decodedString = in.readLine()) != null) {
+                while ((decodedString = in.readLine()) != null) {
                     returnMsg += decodedString;
                     myJson = new JSONObject(returnMsg);
                     String str = myJson.optString("data");
                     str = str.substring(1, str.length() - 1);
-                    str = str.replace(",", "");
-                    str = str.replace(":", "");
+                    str = str.replace(",", " ");
+                    str = str.replace(":", " ");
                     prsdData = str.split("[\"]");
                     len = prsdData.length;
-                }**/
-
-                //Get the instance of JSONArray that contains JSONObjects
-                //jsonArray = myJson.optJSONArray("data");
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
-                    Float value = Float.parseFloat(jsonObject1.optString(jsonArray_keys.getString(i)));  //jsonArray_keys.getString(i) will get time stamp value
-                    data.add(value);
-
-                    /**int id = Integer.parseInt(jsonObject.optString("id").toString());
-                    String name = jsonObject.optString("name").toString();
-                    float salary = Float.parseFloat(jsonObject.optString("salary").toString());
-
-                    data += "Node" + i + " : \n id= " + id + " \n Name= " + name + " \n Salary= " + salary + " \n ";**/
                 }
 
-                in.close();
 
-                 Log.v(LOG_TAG, "Forecast string: " + JsonStr);
+                in.close(); //close stream
+
+                 Log.v(LOG_TAG, "Forecast string: " + returnMsg);
 
             } catch (Exception e) {
                // e.printStackTrace();
@@ -468,21 +170,54 @@ catch (JSONException e){}
                     connection.disconnect();              //connection.disconnect();
                 }
             }
-            //return jsonArray;
             return returnMsg;
         }
 
-        /**protected void onPostExecute(String result) {
-           // LineChart chart = (LineChart) v.findViewById(R.id.chart1);
-            chart.setTouchEnabled(true);
-            chart.setDrawGridBackground(false);
-           // LineData data = new LineData(getXAxisValues(), getDataSet());
-           // data.mChart.setData(data);
-            //chart.setDescription(sensorUID);
-            chart.animateX(900);
-            chart.setPinchZoom(true);
-            chart.getAxisRight().setEnabled(false);
-            chart.invalidate();
-        }**/
+        protected void onPostExecute(String result) {
+           chart = (LineChart) findViewById(R.id.chart1);
+            String str;
+            str=getData();
+            listdata = new ArrayList<>(); //timestamp data for graph
+            listvalue = new ArrayList<>(); //corresponding value of timestamp
+
+
+                int  index_end=0,index_start=0;
+            //this is used to extract data from parsed string
+                    for (int i = 0; i < str.length(); i+=2)
+                    {
+                        while(!(str.charAt(i)==' '))
+                        index_end=str.indexOf(" ",index_end);
+                        String temp=str.substring(index_start,index_end);
+                        listdata.add(temp);
+                        index_start=index_end;
+                        while(!(str.charAt(i)==' '))
+                        index_end=str.indexOf(" ",index_end);
+                        temp=str.substring(index_start,index_end);
+                        listvalue.add(Float.parseFloat(temp));
+                        index_start=index_end;
+                    }
+
+            ArrayList<Date> time = new ArrayList<>(); //will be convert time stamp to real date and time
+            for (int i = 0;i<listdata.size(); i++){
+                Timestamp stamp = new Timestamp(Long.parseLong(listdata.get(i).toString()));
+                Date date = new Date(stamp.getTime());
+                time.add(date);
+            }
+            //used to set entries
+            ArrayList<Entry> values = new ArrayList<>(); //data for y-axis
+            for (int i=0;i<data.size();i++){ //adding data for x and y axis respectively
+                values.add(new Entry(listvalue.get(i),Float.parseFloat(listdata.get(i)))); //
+            }
+
+
+            LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+
+
+            // create a data object with the datasets
+            LineData data = new LineData(set1);
+
+            // set data
+            chart.setData(data);
+        }
     }
 }
